7528fbeb64b1dcdaba5767772b1885eac63f270e
Ben Webb <ben@salilab.org>
2007-03-25 23:46:00 +0000

    Implement features 79 and 83 (atom type).

--- src/mdt_add_alignment.c
+++ src/mdt_add_alignment.c
@@ -51,6 +51,15 @@ static int irestab(const struct f_int2_array *ialn, int naln, int iseq,
   }
 }
 
+static int itable(const int *itab, int nr, int ir, int ndim)
+{
+  if (ir >= 1 && ir <= nr && itab[ir-1] >= 1 && itab[ir-1] <= ndim) {
+    return itab[ir-1];
+  } else {
+    return ndim;
+  }
+}
+
 static int my_mdt_index(int ifi, const struct alignment *aln, int is1, int ip1,
                         int is2, int ir1, int ir2, int ir1p, int ir2p, int ia1,
                         int ia1p, const struct mdt_library *mlib, int ip2,
@@ -58,7 +67,9 @@ static int my_mdt_index(int ifi, const struct alignment *aln, int is1, int ip1,
                         const struct libraries *libs,
                         const struct energy_data *edat, int *ierr)
 {
+  struct structure *struc1;
   struct sequence *seq1, *seq2;
+  struc1 = alignment_structure_get(aln, is1-1);
   seq1 = alignment_sequence_get(aln, is1-1);
   seq2 = alignment_sequence_get(aln, is2-1);
   switch(ifi) {
@@ -78,6 +89,12 @@ static int my_mdt_index(int ifi, const struct alignment *aln, int is1, int ip1,
     return irestab(&aln->ialn, aln->naln, is2, f_int1_pt(&seq2->irestyp),
                    seq2->nres, ip1, mlib->deltaj, mlib->deltaj_ali,
                    mlib->ndimen[ifi-1], libs->igaptyp);
+  case 79:
+    return itable(f_int1_pt(&struc1->iatta), struc1->cd.natm, ia1,
+                  mlib->ndimen[ifi-1]);
+  case 83:
+    return itable(f_int1_pt(&struc1->iatta), struc1->cd.natm, ia1p,
+                  mlib->ndimen[ifi-1]);
   default:
     return mdt_index(ifi, aln, is1, ip1, is2, ir1, ir2, ir1p, ir2p, ia1, ia1p,
                      mlib, ip2, ibnd1, ibnd1p, is3, ir3, ir3p, libs, edat,

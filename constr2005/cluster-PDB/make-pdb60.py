f491c95c5137fa86f9ecb3af8dd58db106880f26
Ben Webb <ben@salilab.org>
2007-04-17 22:09:11 +0000

    Parse pdball.pir in Python rather than reading in a sequence database, since the latter loses some information.

--- constr2005/cluster-PDB/make-pdb60.py
+++ constr2005/cluster-PDB/make-pdb60.py
@@ -1,4 +1,5 @@
 from modeller import *
+import re
 
 log.verbose()
 env = environ()
@@ -10,7 +11,17 @@ sdb.filter(rr_file='${LIB}/blosum62.sim.mat', gap_penalties_1d=(-500, -50),
            matrix_offset = -450, max_diff_res=30, seqid_cut=60, 
            output_grp_file='pdb_60.grp', output_cod_file='pdb_60.cod')
 
-sdb = sequence_db(env, chains_list='pdb_60.cod', seq_database_file='pdball.pir',
-                  seq_database_format='PIR')
-sdb.write(chains_list='ALL', seq_database_file='pdb_60.pir',
-          seq_database_format='PIR')
+# Make pdb_60.pir file by copying every sequence listed in pdb_60.cod
+# from pdball.pir:
+out = file("pdb_60.pir", "w")
+codes = [line.rstrip('\r\n') for line in file("pdb_60.cod")]
+codes = dict.fromkeys(codes)
+
+pirhead = re.compile(">P1;(.*)$")
+printline = False
+for line in file("pdball.pir"):
+    m = pirhead.match(line)
+    if m:
+        printline = m.group(1) in codes
+    if printline:
+        out.write(line)

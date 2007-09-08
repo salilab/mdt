992d1a30d323965552e37bf33409246e855e24de
Ben Webb <ben@salilab.org>
2007-09-08 02:02:27 +0000

    Remove old Latex documentation, and add crude SCons builder for the new epydoc and DocBook documentation.

--- doc/epydoc.sh
+++ doc/epydoc.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-LD_LIBRARY_PATH=../src/ modpy.sh ~/bin/bin/epydoc --inheritance grouped --no-private --no-frames --html $* ../pyext/mdt.py
+prefix=$1
+shift
+LD_LIBRARY_PATH=../src/ ${prefix}modpy.sh ~/bin/bin/epydoc -o html/epydoc --inheritance grouped --no-private --no-frames --html $* ../pyext/mdt.py

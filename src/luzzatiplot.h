7b9edf15095fdb9ead0d2e97e0c0d3e0b56e2f7c
Ben Webb <ben@salilab.org>
2010-03-19 20:55:31 +0000

    Add GQ's pre-calculated Luzzati plot, for error estimation.

---
+++ src/luzzatiplot.h
@@ -0,0 +1,22 @@
+/** \file luzzatiplot.h       Estimate error from R-factor using Luzzati plots.
+ *
+ *             Part of MDT, Copyright(c) 1989-2008 Andrej Sali
+ */
+
+#ifndef __MDT_LUZZATI_PLOT_H
+#define __MDT_LUZZATI_PLOT_H
+
+#include <glib.h>
+#include "mdt_config.h"
+
+G_BEGIN_DECLS
+
+/** Return error from R-factor using a pre-calculated Luzzati plot.
+    The error returned is (error on coordinates)x(1/x-ray resolution)
+ */
+MDTDLLLOCAL
+float luzzatiplot(int ind);
+
+G_END_DECLS
+
+#endif  /* __MDT_LUZZATI_PLOT_H */

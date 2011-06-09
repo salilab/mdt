b39f805cd52b6756d586767420757e4396288c7f
Ben Webb <ben@salilab.org>
2011-06-09 18:22:16 +0000

    Don't add redundant output when using newer unittest; add coverage report.

--- test/run-all-tests.py
+++ test/run-all-tests.py
@@ -1,4 +1,65 @@
 import unittest, sys, os, re
+import glob
+
+# Our bundled version of coverage only works with Python 2
+if sys.version_info[0] < 3:
+    import coverage
+else:
+    coverage = None
+
+try:
+    # Custom TestResult class that does not duplicate test name and docstring
+    # when using unittest2
+    from unittest import TextTestResult
+
+    class _TestRunner(unittest.TextTestRunner):
+        class _TestResult(TextTestResult):
+            def getDescription(self, test):
+                doc_first_line = test.shortDescription()
+                if self.descriptions and doc_first_line:
+                    return doc_first_line
+                else:
+                    return str(test)
+        def _makeResult(self):
+            return self._TestResult(self.stream, self.descriptions,
+                                    self.verbosity)
+except ImportError:
+    # Older Pythons with unittest1 don't export TextTestResult and also
+    # don't duplicate test names
+    _TestRunner = unittest.TextTestRunner
+
+
+class RunAllTests(unittest.TestProgram):
+    """Custom main program that also displays a final coverage report"""
+    def __init__(self, *args, **keys):
+        if coverage:
+            # Start coverage testing now before we import any modules
+            coverage.start()
+
+        # Run the tests
+        unittest.TestProgram.__init__(self, *args, **keys)
+
+    def runTests(self):
+        self.testRunner = _TestRunner(verbosity=self.verbosity)
+        result = self.testRunner.run(self.test)
+
+        if coverage:
+            coverage.stop()
+            coverage.the_coverage.collect()
+            coverage.use_cache(False)
+            print >> sys.stderr, "\nPython coverage report\n"
+
+            # Don't show full paths to modules in coverage output
+            cwd = os.path.dirname(sys.argv[0])
+            topdir = os.path.abspath(os.path.join(cwd, '..', 'pyext')) + '/'
+            coverage.the_coverage.relative_dir = topdir
+
+            mods = [topdir + 'mdt/*.py']
+            coverage.report(mods, file=sys.stderr)
+            for cov in glob.glob('.coverage.*'):
+                os.unlink(cov)
+        sys.exit(not result.wasSuccessful())
+
 
 def regressionTest():
     path = os.path.abspath(os.path.dirname(sys.argv[0]))
@@ -12,4 +73,4 @@ def regressionTest():
     return unittest.TestSuite(tests)
 
 if __name__ == "__main__":
-    unittest.main(defaultTest="regressionTest")
+    RunAllTests(defaultTest="regressionTest")

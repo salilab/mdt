45bc74784337f98d6932476da62a02542394c36a
Ben Webb <ben@salilab.org>
2015-09-04 10:06:44 -0700

    Provide mock Python modules for building docs.
    This should allow us to build the docs without needing Modeller
    or the _mdt C extension.

--- doc/conf.py
+++ doc/conf.py
@@ -25,6 +25,7 @@ def get_version():
 # If your extensions are in another directory, add it here. If the directory
 # is relative to the documentation root, use os.path.abspath to make it
 # absolute, like shown here.
+sys.path.append(os.path.abspath('mockpy'))
 sys.path.append(os.path.abspath('../pyext'))
 
 # General configuration

94bf00f26136a946c8d0641065c50acdfc3029ec
Ben Webb <benmwebb@gmail.com>
2025-10-12 23:12:10 -0700

    Fix typo

--- tools/travis/install_deps.sh
+++ tools/travis/install_deps.sh
@@ -22,7 +22,7 @@ modeller_url=https://salilab.org/modeller/${MODELLER_VERSION}
 wget ${modeller_url}/modeller_${MODELLER_VERSION}-1_amd64.deb
 sudo --preserve-env=KEY_MODELLER dpkg -i modeller_${MODELLER_VERSION}-1_amd64.deb
 # Modeller 10.6 only supports up to Python 3.13, but 3.14 should also work
-sudo mkdir -p /usr/lib/python3.13/dist-packages/
+sudo mkdir -p /usr/lib/python3.14/dist-packages/
 sudo ln -sf /usr/lib/modeller${MODELLER_VERSION}/lib/x86_64-intel8/python3.3/_modeller.so /usr/lib/python3.14/dist-packages/
 sudo ln -sf /usr/lib/modeller${MODELLER_VERSION}/modlib/modeller /usr/lib/python3.14/dist-packages/
 
